bd608f4972daedbaea7fcd45fd6c881ff8d3d43c
Lucas Soares Caetano <caetanolucassoares@gmail.com>
2024-12-02 15:49:16 -0300

    Adiciona rotas de criacao e listagem de atores. inicia cadastro

---
+++ app/controllers/actors_controller.rb
@@ -0,0 +1,21 @@
+class ActorsController < ApplicationController
+
+  def new
+    @actor = Actor.new
+  end
+  def create
+    @actor = Actor.new(actor_params)
+    if @actor.save
+      redirect_to actors_path
+    end
+  end
+  def index
+    @actors = Actor.all
+  end
+
+  private 
+
+  def actor_params
+    params.require(:actor).permit(:name, :nationality, :birth)
+  end
+end
--- spec/system/user_register_actor_spec.rb
+++ spec/system/user_register_actor_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
 
-describe 'usuario registra um novo autor' do
+describe 'usuario registra um novo ator' do
   it 'com sucesso' do
     user = FactoryBot.create(:user, email: 'joao@email.com')
     login_as user
 
     visit root_path
     click_on 'Atores'
-    click_on 'Cadastrar novo Autor'
+    click_on 'Cadastrar novo Ator'
 
     fill_in 'Nome', with: 'Ator1'
     fill_in 'Nacionalidade', with: 'Brasileiro'
@@ -18,6 +18,6 @@ describe 'usuario registra um novo autor' do
     expect(page).to have_content 'Brasileiro'
     expect(page).to have_content '1992'
     expect(current_path).to eq detalhesdoautoraqui_path
-
+    
   end
 end
